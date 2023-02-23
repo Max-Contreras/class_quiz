fe8a936fe771af0f75212f81f7052add4c1ddfa7
Max-Contreras <121184009+Max-Contreras@users.noreply.github.com>
2023-02-23 02:59:52 +0000

    FInally Finished

--- card.py
+++ card.py
@@ -1,6 +1,14 @@
-# Name:
+# Name: Max Contreras
 
 class Card:
-    """class representing a simple Neapolitan card. Holds a 
-    value and a suit"""
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+    def __repr__(self):
+        return f"Card(value = {self.value}, suit = {self.suit})"
+    def __str__(self):
+        return f"{self.value} of {self.suit}"
+    def __eq__(self, other):
+        if self.value == other.value:
+            return "True"
     
